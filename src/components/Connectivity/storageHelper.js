a71944e35001062c333563b5b64062c14be2f88a
Mannan <Mannan@DESKTOP-OTIQOF5>
2025-01-18 15:44:20 +0530

    chore: Update storageHelper.js to use new base URL

--- src/components/Connectivity/storageHelper.js
+++ src/components/Connectivity/storageHelper.js
@@ -27,7 +27,7 @@ function getTodayDate() {
 function getBaseUrl() {
     // window.BaseurlName = import.meta.env.VITE_BACKEND_API_URL;
     // window.BaseurlName = "https://apiudyansaathi.azurewebsites.net/api/";
-    window.BaseurlName = "https://xnv320z0-8000.inc1.devtunnels.ms/api/get-GraphData/api/";
+    window.BaseurlName = "https://xnv320z0-8000.inc1.devtunnels.ms/api/";
     return window.BaseurlName || '';
 }
 
